276f8aa2ef89a7980b3a5381d73d8d4d81eae82b
Jorge C. Pereira <64277389+jp2435@users.noreply.github.com>
2023-03-04 11:42:46 +0000

    Class03/ex05 Add String Comparison

--- class03/ex05.cpp
+++ class03/ex05.cpp
@@ -17,9 +17,11 @@ int main(){
     double Da=4.4,Db=3.9;
     float Fa=3.2,Fb=3.1999999;
     int Ia=10,Ib=10;
+    string a="abc",b="abcd";
 
     cout << "Largest Double: " << Largest(Da,Db) << endl
          << "Largest Float: " << Largest(Fa,Fb) << endl
-         << "Largest Int: " << Largest(Ia,Ib);
+         << "Largest Int: " << Largest(Ia,Ib) << endl
+         << "Largest String:" << Largest(a,b);
     return 0;
 }
