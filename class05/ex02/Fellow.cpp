6dd479d060318269346b3b068d84bb01e41ef768
Jorge C. Pereira <64277389+jp2435@users.noreply.github.com>
2023-03-15 18:38:25 +0000

    Class05/ex2 Fellow Class Getter Member Function

---
+++ class05/ex02/Fellow.cpp
@@ -0,0 +1,10 @@
+#include "Fellow.h"
+
+
+
+// Setter Member Functions
+
+// Getter Member Functions
+double Fellow::getScholarship() const{
+    return scholarship_;
+}
--- class05/ex02/Fellow.h
+++ class05/ex02/Fellow.h
@@ -3,12 +3,18 @@
 
 #include "./Student.h"
 
+// Bolseiros
 class Fellow: public Student {
 private:
     double scholarship_;
 public:
     Fellow(const std::string &name,unsigned int cc, const Date& birth,const Date& enroll=CurrentTime(),double scholarship=1000.00):
         Student(name,cc,birth,enroll), scholarship_(scholarship){}
+    ~Fellow() = default;
+    // Setter Member Functions
+
+    // Getter Member Functions
+    double getScholarship() const;
 };
 
 
